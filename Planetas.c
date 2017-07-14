88834cd7718fcee4b382f72c87559a6f26ddc547
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-13 19:27:26 -0500

    Intento en clase de empezar a hacer el ejercicio

---
+++ Planetas.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define G 39.4783
+
+float get_gravedad(float masa);
+
+int main(void){
+	int t = 100;
+	print_algo(t);	
+	//int var;
+	//FILE *in;
+	//char filename[100]="coordinates.csv";
+
+	//in = fopen(filename, "r");
+	//for(i=0;i<20;i++){
+	//	fscanf(in, "%d\n", &var);
+	//	printf("value = %d\n",var);
+	//}
+	//fclose(in);
+	return 0;
+	}
+
+void print_algo(t){
+	int i;
+	float masa;
+	float poxMerc[t];
+	float poyMerc[t];
+	float pozMerc[t];
+	float vxMerc[t];
+	float vyMerc[t];
+	float vzMerc[t];	
+
+	//Suponemos que el sol no se mueve
+	float PosicionSolx = 0.0;
+	float PosicionSoly = 0.0;
+	float PosicionSolz = 0.0;
+	
+	float poxMerc[0]=0.3615;
+	float poyMerc[0]=-0.027;
+	float pozMerc[0]=-0.0355;
+	
+	float vxMerc[0]=-1.11846;
+	float vyMerc[0]=10.7016;
+	float vxMerc[0]=0.9768;
+
+	for (i=1; i<t;i++){
+		poxMerc[i]= 
+	}
+	return 0;
+	
+
+}
+
+	
