dea2368215a3842e384471c068c36f9ab4723bea
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-15 13:31:03 -0500

    Plot en 3d de la primera perturbacion

--- Onda.py
+++ Onda.py
@@ -1,31 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 a = -15.0
 b = 15.0
-h=0.1
-puntos= (b-a)/(h)
+puntos= 100
+h= (b-a)/(puntos)
 c=1.0
 gamma = c*(0.0005)/h
 tiempo = 60
-lamda = np.pi/2
-#Numero de onda
-k=np.pi*2/lamda
-w = np.pi*2/tiempo
 
-t = np.linspace(a,b,puntos)
-#x = np.linspace(a,b,puntos)
-#y = np.linspace(a,b,puntos)
 
-amplitud = np.zeros((30, 30))
+x = np.linspace(a,b, puntos)
+y = np.linspace(a,b, puntos)
+
+amplitud0 = np.zeros((int(puntos), int(puntos)))
+
 #Inicial
-amplitud[10][15]= -0.5
+amplitud0[int(puntos/3)][int(puntos/2)]= -0.5
 
 
-#fi0 = np.sin(k*xv - w*t)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X,Y = np.meshgrid(x,y)
+surf = ax.plot_surface(X,Y, amplitud0[:], rstride=1, cstride=1, color='c')
+ax.set_xlim(-15,15)
+ax.set_ylim(-15,15)
+ax.set_zlim(-2,0)
 
-plt.imshow(amplitud)
 plt.show()
+
+
+
+
+
+
+
 
 
