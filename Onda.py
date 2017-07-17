5e20de56724bf399b6a519f31208f2ff1ff638d6
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-16 20:03:19 -0500

    ultimo intento el domingo :(

--- Onda.py
+++ Onda.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
-a = 0.0
-b = 30.0
+a = -15.0
+b = 15.0
 puntos= 30
 dx = (b-a)/(puntos)
 dy = (b-a)/(puntos)
@@ -29,43 +29,40 @@ amplitud0 = np.zeros((30, 30))
 #Inicial
 #amplitud0[int(puntos/3)][int(puntos/2)]= -0.5
 amplitud0[10][15]= -0.5
+AInicial = np.copy(amplitud0)
 
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#X,Y = np.meshgrid(x,y)
-#surf = ax.plot_surface(X,Y, amplitud0[:], rstride=1, cstride=1, color='c')
-#ax.set_xlim(-15,15)
-#ax.set_ylim(15,-15)
-#ax.set_zlim(-2,0)
-
-#plt.show()
-plt.close()
 
 amplitud1 = np.zeros((30, 30))
 amplitud1[10][15]= -0.5
-		
+
+
+#i es 1:-1
+#i+1 es 2:
+#i-1 es 0:-2
 for i in range(tiempo+1):
 	amplitud1[1:-1,1:-1]= 2*amplitud0[1:-1,1:-1]+ rx*rx/2*(amplitud0[2:,1:-1]-2*amplitud0[1:-1,1:-1]+amplitud0[0:-2,1:-1]) + ry*ry/2*(amplitud0[1:-1,2:]-2*amplitud0[1:-1,1:-1]+amplitud0[1:-1, 0:-2]) 
 
 amplitudFinal = np.zeros((30,30))
-amplitudFinal[10][15]= -0.5
+
 
 AinTime=[]
 time=[]
 t=0
 while t<tiempo:
 	time.append(t)
+	#amplitud0[10][15]= -0.5*np.sin(30+np.pi*t/20.0)
 	for i in range(tiempo+1):
 		amplitudFinal[1:-1,1:-1]=2*(1-rx*rx)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+rx*rx*(amplitud1[2:,1:-1]+amplitud1[0:-2,1:-1]) + 2*(1-ry*ry)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+ry*ry*(amplitud1[1:-1,2:]+amplitud1[1:-1,0:-2])
 
 	amplitud0 = np.copy(amplitud1)
 	amplitud1 = np.copy(amplitudFinal)
+	AinTime.append(amplitudFinal)
 	t+=1
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 X,Y = np.meshgrid(x,y)
-surf = ax.plot_surface(X,Y, amplitudFinal[:], rstride=1, cstride=1, color='c')
+surf = ax.plot_surface(X,Y, amplitudFinal, rstride=1, cstride=1, color='c')
 ax.set_xlim(-15,15)
 ax.set_ylim(15,-15)
 ax.set_zlim(-2,0)
