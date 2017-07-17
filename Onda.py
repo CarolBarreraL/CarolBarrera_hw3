95d2c7b684f2143e66101ff30b7ba01181b3ce86
CarolBarreral <cv.barrera10@uniandes.edu.co>
2017-07-17 10:41:33 -0500

    Intento de otra manera, pero no funciono tampoco :(

--- Onda.py
+++ Onda.py
@@ -51,6 +51,7 @@ for i in range(tiempo+1):
 	amplitud0[:,-1]=0
 
 
+
 amplitudFinal = np.zeros((puntos,puntos))
 
 
@@ -59,20 +60,42 @@ time=[]
 t=0
 AinTime.append(amplitud0)
 AinTime.append(amplitud1)
+#while t<tiempo:
+#	time.append(t)
+#	for i in range(tiempo+1):
+#		amplitudFinal[1:-1,1:-1]=2*(1-rx*rx)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+rx*rx*(amplitud1[2:,1:-1]+amplitud1[0:-2,1:-1]) + 2*(1-ry*ry)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+ry*ry*(amplitud1[1:-1,2:]+amplitud1[1:-1,0:-2])
+	
+#	amplitudFinal[0,:]=0
+#	amplitudFinal[:,0]=0
+#	amplitudFinal[-1,:]=0
+#	amplitudFinal[:,-1]=0
+	
+#	amplitud0 = np.copy(amplitud1)
+#	amplitud1 = np.copy(amplitudFinal)
+#	AinTime.append(amplitudFinal)
+#	t+=1
+
+#Intento de otra manera
 while t<tiempo:
 	time.append(t)
-	for i in range(tiempo+1):
-		amplitudFinal[1:-1,1:-1]=2*(1-rx*rx)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+rx*rx*(amplitud1[2:,1:-1]+amplitud1[0:-2,1:-1]) + 2*(1-ry*ry)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+ry*ry*(amplitud1[1:-1,2:]+amplitud1[1:-1,0:-2])
-
+	for i in range(1,puntos-1):
+		amplitudFinal[i,i]=2*(1-rx*rx)*amplitud1[i,i]-amplitud0[i,i]+rx*rx*(amplitud1[i+1,i]+amplitud1[i-1,i]) + 2*(1-ry*ry)*amplitud1[i,i]-amplitud0[i,i]+ry*ry*(amplitud1[i,i+1]+amplitud1[i,i-1])
+	
+	amplitudFinal[0,:]=0
+	amplitudFinal[:,0]=0
+	amplitudFinal[-1,:]=0
+	amplitudFinal[:,-1]=0
+	
 	amplitud0 = np.copy(amplitud1)
 	amplitud1 = np.copy(amplitudFinal)
 	AinTime.append(amplitudFinal)
 	t+=1
 
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 X,Y = np.meshgrid(x,y)
-surf = ax.plot_surface(X,Y, AInicial, color='c', cmap = cm.coolwarm)
+surf = ax.plot_surface(X,Y, AinTime[2], color='c', cmap = cm.coolwarm)
 fig.colorbar(surf, shrink =0.5, aspect = 5)
 ax.set_xlim(-15,15)
 ax.set_ylim(15,-15)
@@ -81,17 +104,19 @@ plt.show()
 
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')
-#linea, = ax.plot([],[],[], label='Onda', lw=0.5)
+#linea = ax.plot_surface(X,Y, AinTime[10], color='c', cmap = cm.coolwarm)
 #ax.set_xlim(-15,15)
 #ax.set_ylim(15,-15)
-#ax.set_zlim(-2,0)
+#ax.set_zlim(-2,1)
 
-#def anima():
-#	linea.set_data(x,y)
-#	linea.set_3d_properties(amplitudFinal)
+#def anima(i):
+#	amplitud = AinTime[i]
+#	ax.clear()	
+#	linea = ax.plot_surface(X,Y,amplitud, cmap = cm.coolwarm)
+#	ax.set_zlim(-2,1)
 #	return linea,
 
-#anim = animation.FuncAnimation(fig, anima, frames=500, fargs=(0,1,1.05), interval=5, blit=True)
+#anim = animation.FuncAnimation(fig, anima, tiempo, fargs=(0,1,1.05), interval=25, blit=False)
 #anim.save('Onda.mp4', fps = 10)
 
 
