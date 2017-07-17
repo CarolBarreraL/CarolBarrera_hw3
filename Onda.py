674d35ab9a6d47d7aacfb2329b81706ebd90c504
CarolBarreral <cv.barrera10@uniandes.edu.co>
2017-07-17 10:17:16 -0500

    Creo que ahora estan bien las condiciones iniciales

--- Onda.py
+++ Onda.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
+from matplotlib import cm
 
 a = -15.0
 b = 15.0
@@ -14,27 +15,29 @@ ry = c*(0.5)/dy
 tiempo = 60
 
 
-#x = np.linspace(a,b, puntos)
-#y = np.linspace(a,b, puntos)
-x = np.linspace(a,b, 30)
-y = np.linspace(a,b, 30)
+x = np.linspace(a,b, puntos)
+y = np.linspace(a,b, puntos)
+
 
 
 
 #Matriz con ampitudes
-#amplitud0 = np.zeros((int(puntos), int(puntos)))
-amplitud0 = np.zeros((30, 30))
+amplitud0 = np.zeros((puntos, puntos))
 
 
-#Inicial
-#amplitud0[int(puntos/3)][int(puntos/2)]= -0.5
-amplitud0[10][15]= -0.5
+#Perturbacion inicial y condiciones iniciales
+amplitud0[int((b-a)/3),int((b-a)/2)]= -0.5
+#En los bordes
+amplitud0[0,:]=0
+amplitud0[:,0]=0
+amplitud0[-1,:]=0
+amplitud0[:,-1]=0
 AInicial = np.copy(amplitud0)
 
 
-amplitud1 = np.zeros((30, 30))
-amplitud1[10][15]= -0.5
-
+amplitud1 = np.zeros((puntos, puntos))
+#Condicion inicial
+amplitud1[int((b-a)/3),int((b-a)/2)]= -0.5
 
 #i es 1:-1
 #i+1 es 2:
@@ -42,15 +45,22 @@ amplitud1[10][15]= -0.5
 for i in range(tiempo+1):
 	amplitud1[1:-1,1:-1]= 2*amplitud0[1:-1,1:-1]+ rx*rx/2*(amplitud0[2:,1:-1]-2*amplitud0[1:-1,1:-1]+amplitud0[0:-2,1:-1]) + ry*ry/2*(amplitud0[1:-1,2:]-2*amplitud0[1:-1,1:-1]+amplitud0[1:-1, 0:-2]) 
 
-amplitudFinal = np.zeros((30,30))
+	amplitud0[0,:]=0
+	amplitud0[:,0]=0
+	amplitud0[-1,:]=0
+	amplitud0[:,-1]=0
+
+
+amplitudFinal = np.zeros((puntos,puntos))
 
 
 AinTime=[]
 time=[]
 t=0
+AinTime.append(amplitud0)
+AinTime.append(amplitud1)
 while t<tiempo:
 	time.append(t)
-	#amplitud0[10][15]= -0.5*np.sin(30+np.pi*t/20.0)
 	for i in range(tiempo+1):
 		amplitudFinal[1:-1,1:-1]=2*(1-rx*rx)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+rx*rx*(amplitud1[2:,1:-1]+amplitud1[0:-2,1:-1]) + 2*(1-ry*ry)*amplitud1[1:-1,1:-1]-amplitud0[1:-1,1:-1]+ry*ry*(amplitud1[1:-1,2:]+amplitud1[1:-1,0:-2])
 
@@ -62,10 +72,11 @@ while t<tiempo:
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 X,Y = np.meshgrid(x,y)
-surf = ax.plot_surface(X,Y, amplitudFinal, rstride=1, cstride=1, color='c')
+surf = ax.plot_surface(X,Y, AInicial, color='c', cmap = cm.coolwarm)
+fig.colorbar(surf, shrink =0.5, aspect = 5)
 ax.set_xlim(-15,15)
 ax.set_ylim(15,-15)
-ax.set_zlim(-2,0)
+ax.set_zlim(-1,0)
 plt.show()
 
 #fig = plt.figure()
