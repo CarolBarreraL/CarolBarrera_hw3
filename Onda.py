44f3f4e847cb5af6beb5023274da349c81ff50b6
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-18 23:18:39 -0500

    Funcionooooo, pero falta ver animacion :(

--- Onda.py
+++ Onda.py
@@ -13,7 +13,7 @@ c=1.0
 r = 0.5
 dt = np.sqrt(r)*dx/c
 tiempo = 60
-
+iteraciones = 60/dt
 
 x = np.linspace(a,b, puntos)
 y = np.linspace(a,b, puntos)
@@ -30,12 +30,23 @@ amplitud0[0,:]=0
 amplitud0[:,0]=0
 amplitud0[-1,:]=0
 amplitud0[:,-1]=0
+#Rendija
+hueco = 3
+amplitud0[int((b-a)/3)*2][:int((b-a)/2)-hueco]= 0
+amplitud0[int((b-a)/3)*2][int((b-a)/2)+hueco:]= 0
 AInicial = np.copy(amplitud0)
 
 
 amplitud1 = np.zeros((puntos, puntos))
 #Condicion inicial
 amplitud1[int((b-a)/3)][int((b-a)/2)]= -0.5
+amplitud1[0,:]=0
+amplitud1[:,0]=0
+amplitud1[-1,:]=0
+amplitud1[:,-1]=0
+#Rendija
+amplitud1[int((b-a)/3)*2][:int((b-a)/2)-hueco]= 0
+amplitud1[int((b-a)/3)*2][int((b-a)/2)+hueco:]= 0
 
 #i es 1:-1
 #i+1 es 2:
@@ -55,54 +66,60 @@ amplitudFinal = np.zeros((puntos,puntos))
 
 
 AinTime=[]
-time=[]
 t=0
 AinTime.append(amplitud0)
 AinTime.append(amplitud1)
-while t<tiempo:
-	time.append(t)
+while t<int(iteraciones):
 	for i in range(1,puntos-1):
 		for j in range(1,puntos-1):
-		amplitudFinal[1:-1,1:-1]=2*amplitud1[i,j]-amplitud0[i,j]+r*(amplitud1[i+1,j]+amplitud1[i-1,j]) + r*(amplitud1[i,j+1]+amplitud1[i,j-1])
-	
+			amplitudFinal[i,j]=2*amplitud1[i,j]-amplitud0[i,j]+r*(amplitud1[i+1,j]-2*amplitud1[i,j]+amplitud1[i-1,j])+r*(amplitud1[i,j+1]-2*amplitud1[i,j]+amplitud1[i,j-1])
+	#Condiciones de frontera
 	amplitudFinal[0,:]=0
 	amplitudFinal[:,0]=0
 	amplitudFinal[-1,:]=0
 	amplitudFinal[:,-1]=0
 	
+	#Rendija
+	amplitudFinal[int((b-a)/3)*2][:int((b-a)/2)-hueco]= 0
+	amplitudFinal[int((b-a)/3)*2][int((b-a)/2)+hueco:]= 0
+
+	#Evolucion
 	amplitud0 = np.copy(amplitud1)
 	amplitud1 = np.copy(amplitudFinal)
-	AinTime.append(amplitudFinal)
+	AinTime.append(amplitud1)
 	t+=1
 
+itPara30 = 30/dt
 
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+#ax = fig.gca(projection='3d')
 X,Y = np.meshgrid(x,y)
-surf = ax.plot_surface(X,Y, AinTime[10], color='c', cmap = cm.coolwarm)
-fig.colorbar(surf, shrink =0.5, aspect = 5)
+ax = fig.add_subplot(111, projection='3d')
+linea = ax.plot_surface(X,Y, AinTime[20],linewidth =0, color='c', cmap = cm.coolwarm)
+fig.colorbar(linea, shrink =0.5, aspect = 5)
 ax.set_xlim(-15,15)
 ax.set_ylim(15,-15)
-ax.set_zlim(-1,0)
+ax.set_zlim(-2,1)
 plt.show()
+plt.close()
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#linea = ax.plot_surface(X,Y, AinTime[10], color='c', cmap = cm.coolwarm)
-#ax.set_xlim(-15,15)
-#ax.set_ylim(15,-15)
-#ax.set_zlim(-2,1)
+plt.imshow(AinTime[20])
+plt.colorbar()
+plt.show()
+plt.close()
 
-#def anima(i):
-#	amplitud = AinTime[i]
-#	ax.clear()	
-#	linea = ax.plot_surface(X,Y,amplitud, cmap = cm.coolwarm)
-#	ax.set_zlim(-2,1)
-#	return linea,
 
-#anim = animation.FuncAnimation(fig, anima, tiempo, fargs=(0,1,1.05), interval=25, blit=False)
-#anim.save('Onda.mp4', fps = 10)
+def anima(i):
+	amplitud = AinTime[i]
+	ax.clear()	
+	linea = ax.plot_surface(X,Y,amplitud, cmap = cm.coolwarm)
+	#linea = plt.imshow(amplitud)
+	ax.set_zlim(-2,1)
+	return linea,
+
+#anim = animation.FuncAnimation(fig, anima, int(iteraciones), interval=25, blit=False)
+#anim.save('Onda.mp4', fps = 20)
 
 
 
