d02a6fe4470ae67314acc3ce1476e020e680d89a
CarolBarreral <cv.barrera10@uniandes.edu.co>
2017-07-19 17:22:55 -0500

    Arregle las condiciones iniciales, porque lo tenia mal

--- Onda.py
+++ Onda.py
@@ -4,11 +4,11 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 from matplotlib import cm
 
-a = -15.0
-b = 15.0
-puntos= 30
-dx = (b-a)/(puntos)
-dy = (b-a)/(puntos)
+a =0
+b =30.0
+puntos= 60
+dx = (b-a)/(puntos-1)
+dy = (b-a)/(puntos-1)
 c=1.0
 r = 0.5
 dt = np.sqrt(r)*dx/c
@@ -24,7 +24,9 @@ amplitud0 = np.zeros((puntos, puntos))
 
 
 #Perturbacion inicial y condiciones iniciales
-amplitud0[int((b-a)/3)][int((b-a)/2)]= -0.5
+#amplitud0[int((b-a)/3)][int((b-a)/2)]= -0.5
+amplitud0[puntos/3][puntos/2]= -0.5
+
 #En los bordes
 amplitud0[0,:]=0
 amplitud0[:,0]=0
@@ -32,21 +34,22 @@ amplitud0[-1,:]=0
 amplitud0[:,-1]=0
 #Rendija
 hueco = 3
-amplitud0[int((b-a)/3)*2][:int((b-a)/2)-hueco]= 0
-amplitud0[int((b-a)/3)*2][int((b-a)/2)+hueco:]= 0
+amplitud0[(puntos/3)*2][:(puntos/2)-hueco]= 0
+amplitud0[(puntos/3)*2][(puntos/2)+hueco:]= 0
+
 AInicial = np.copy(amplitud0)
 
 
 amplitud1 = np.zeros((puntos, puntos))
 #Condicion inicial
-amplitud1[int((b-a)/3)][int((b-a)/2)]= -0.5
+amplitud1[puntos/3][puntos/2]= -0.5
 amplitud1[0,:]=0
 amplitud1[:,0]=0
 amplitud1[-1,:]=0
 amplitud1[:,-1]=0
 #Rendija
-amplitud1[int((b-a)/3)*2][:int((b-a)/2)-hueco]= 0
-amplitud1[int((b-a)/3)*2][int((b-a)/2)+hueco:]= 0
+amplitud1[(puntos/3)*2][:(puntos/2)-hueco]= 0
+amplitud1[(puntos/3)*2][(puntos/2)+hueco:]= 0
 
 #i es 1:-1
 #i+1 es 2:
@@ -55,10 +58,10 @@ for i in range(1,puntos-1):
 	for j in range(1,puntos-1):
 		amplitud1[i,j]= amplitud0[i,j]+ r*r/2*(amplitud0[i+1,j]-2*amplitud0[i,j]+amplitud0[i-1,j]) + r*r/2*(amplitud0[i,j+1]-2*amplitud0[i,j]+amplitud0[i, j-1]) 
 
-	amplitud0[0,:]=0
-	amplitud0[:,0]=0
-	amplitud0[-1,:]=0
-	amplitud0[:,-1]=0
+amplitud0[0,:]=0
+amplitud0[:,0]=0
+amplitud0[-1,:]=0
+amplitud0[:,-1]=0
 
 
 
@@ -80,8 +83,8 @@ while t<int(iteraciones):
 	amplitudFinal[:,-1]=0
 	
 	#Rendija
-	amplitudFinal[int((b-a)/3)*2][:int((b-a)/2)-hueco]= 0
-	amplitudFinal[int((b-a)/3)*2][int((b-a)/2)+hueco:]= 0
+	amplitudFinal[(puntos/3)*2][:(puntos/2)-hueco]= 0
+	amplitudFinal[(puntos/3)*2][(puntos/2)+hueco:]= 0
 
 	#Evolucion
 	amplitud0 = np.copy(amplitud1)
@@ -96,15 +99,15 @@ fig = plt.figure()
 #ax = fig.gca(projection='3d')
 X,Y = np.meshgrid(x,y)
 ax = fig.add_subplot(111, projection='3d')
-linea = ax.plot_surface(X,Y, AinTime[20],linewidth =0, color='c', cmap = cm.coolwarm)
+linea = ax.plot_surface(X,Y, AinTime[25],rstride=1, cstride=1, color='c', cmap = cm.coolwarm)
 fig.colorbar(linea, shrink =0.5, aspect = 5)
-ax.set_xlim(-15,15)
-ax.set_ylim(15,-15)
+#ax.set_xlim(-15,15)
+#ax.set_ylim(15,-15)
 ax.set_zlim(-2,1)
 plt.show()
 plt.close()
 
-plt.imshow(AinTime[20])
+plt.imshow(AinTime[25])
 plt.colorbar()
 plt.show()
 plt.close()
