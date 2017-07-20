497e4733e02a0d1a462f456113ff28c6a59c74cc
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-20 17:34:13 -0500

    intento de plotear todos los planetas

--- Plots_Planetas.py
+++ Plots_Planetas.py
@@ -9,7 +9,7 @@ datos = np.genfromtxt('datos.csv', delimiter=',')
 Posicionesx = datos[:,0]
 Posicionesy = datos[:,1]
 Posicionesz = datos[:,2]
-tiempoT =  3120
+tiempoT =  len(Posicionesx)
 tiempoPlanetas = tiempoT/10
 
 Sol = 0
@@ -28,9 +28,9 @@ matrizPosicionesy = np.zeros((10,tiempoPlanetas))
 matrizPosicionesz = np.zeros((10,tiempoPlanetas))
 
 
-for i in range(10):
-	for j in range(tiempoPlanetas):
-		posicion = i + j*10
+for i in range(0,10):
+	for j in range(0,tiempoPlanetas):
+		posicion = int(i + j*10)
 		matrizPosicionesx[i][j]= Posicionesx[posicion]
 		matrizPosicionesy[i][j]= Posicionesy[posicion]
 		matrizPosicionesz[i][j]= Posicionesz[posicion]
@@ -38,14 +38,31 @@ for i in range(10):
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-linea = ax.plot(matrizPosicionesx[Tierra],matrizPosicionesy[Tierra], matrizPosicionesz[Tierra])
-#fig.colorbar(linea, shrink =0.5, aspect = 5)
-#ax.set_xlim(-15,15)
-#ax.set_ylim(15,-15)
-#ax.set_zlim(-2,1)
-plt.show()
+linea = ax.plot(matrizPosicionesx[Sol],matrizPosicionesy[Sol], matrizPosicionesz[Sol], 'y')
+linea = ax.plot(matrizPosicionesx[Merc],matrizPosicionesy[Merc], matrizPosicionesz[Merc])
+linea = ax.plot(matrizPosicionesx[Ven],matrizPosicionesy[Ven], matrizPosicionesz[Ven])
+linea = ax.plot(matrizPosicionesx[Tierra],matrizPosicionesy[Tierra], matrizPosicionesz[Tierra], 'c')
+linea = ax.plot(matrizPosicionesx[Marte],matrizPosicionesy[Marte], matrizPosicionesz[Marte], 'r')
+linea = ax.plot(matrizPosicionesx[Jup],matrizPosicionesy[Jup], matrizPosicionesz[Jup])
+linea = ax.plot(matrizPosicionesx[Sat],matrizPosicionesy[Sat], matrizPosicionesz[Sat])
+linea = ax.plot(matrizPosicionesx[Uran],matrizPosicionesy[Uran], matrizPosicionesz[Uran], 'b')
+linea = ax.plot(matrizPosicionesx[Nept],matrizPosicionesy[Nept], matrizPosicionesz[Nept])
+linea = ax.plot(matrizPosicionesx[Pluto],matrizPosicionesy[Pluto], matrizPosicionesz[Pluto], 'k')
+
+
+ax.set_xlabel('Unidades Astronomicas (UA)')
+plt.title('Sistema Solar')
+#plt.show()
+plt.savefig('sistemaSolar.pdf')
 plt.close()
-	
+
+
+
+
+
+
+
+
 
 
 
