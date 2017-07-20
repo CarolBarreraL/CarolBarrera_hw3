104d9af0f6298ca6bc11f8a8e770822a798d75f5
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-20 02:24:01 -0500

    Ya esta lista, falta revisar la animacion

--- Onda.py
+++ Onda.py
@@ -6,7 +6,7 @@ from matplotlib import cm
 
 a =0
 b =30.0
-puntos= 60
+puntos= 300
 dx = (b-a)/(puntos-1)
 dy = (b-a)/(puntos-1)
 c=1.0
@@ -18,14 +18,14 @@ iteraciones = 60/dt
 x = np.linspace(a,b, puntos)
 y = np.linspace(a,b, puntos)
 
-
+fig = plt.figure()
 #Matriz con ampitudes
 amplitud0 = np.zeros((puntos, puntos))
 
 
 #Perturbacion inicial y condiciones iniciales
-#amplitud0[int((b-a)/3)][int((b-a)/2)]= -0.5
 amplitud0[puntos/3][puntos/2]= -0.5
+
 
 #En los bordes
 amplitud0[0,:]=0
@@ -43,6 +43,7 @@ AInicial = np.copy(amplitud0)
 amplitud1 = np.zeros((puntos, puntos))
 #Condicion inicial
 amplitud1[puntos/3][puntos/2]= -0.5
+
 amplitud1[0,:]=0
 amplitud1[:,0]=0
 amplitud1[-1,:]=0
@@ -56,7 +57,7 @@ amplitud1[(puntos/3)*2][(puntos/2)+hueco:]= 0
 #i-1 es 0:-2
 for i in range(1,puntos-1):
 	for j in range(1,puntos-1):
-		amplitud1[i,j]= amplitud0[i,j]+ r*r/2*(amplitud0[i+1,j]-2*amplitud0[i,j]+amplitud0[i-1,j]) + r*r/2*(amplitud0[i,j+1]-2*amplitud0[i,j]+amplitud0[i, j-1]) 
+		amplitud1[i,j]= amplitud0[i,j]+ 0.5*r*(amplitud0[i+1,j]-2*amplitud0[i,j]+amplitud0[i-1,j]) + 0.5*r*(amplitud0[i,j+1]-2*amplitud0[i,j]+amplitud0[i, j-1]) 
 
 amplitud0[0,:]=0
 amplitud0[:,0]=0
@@ -91,38 +92,57 @@ while t<int(iteraciones):
 	amplitud1 = np.copy(amplitudFinal)
 	AinTime.append(amplitud1)
 	t+=1
-
-itPara30 = 30/dt
-
-
-fig = plt.figure()
-#ax = fig.gca(projection='3d')
 X,Y = np.meshgrid(x,y)
-ax = fig.add_subplot(111, projection='3d')
-linea = ax.plot_surface(X,Y, AinTime[25],rstride=1, cstride=1, color='c', cmap = cm.coolwarm)
-fig.colorbar(linea, shrink =0.5, aspect = 5)
-#ax.set_xlim(-15,15)
-#ax.set_ylim(15,-15)
+
+#para t=30
+itPara30 = 30/dt
+fig1 = plt.figure(figsize=plt.figaspect(2.))
+fig1.suptitle('Cuando t=30')
+
+ax = fig1.add_subplot(2,1,1, projection='3d')
+linea = ax.plot_surface(X,Y, AinTime[int(itPara30)],rstride=1, cstride=1, cmap = cm.winter)
+fig1.colorbar(linea, shrink =0.5, aspect = 5)
 ax.set_zlim(-2,1)
-plt.show()
+
+ax = fig1.add_subplot(2,1,2)
+ax.imshow(AinTime[int(itPara30)], cmap=cm.winter)
+cM = ax.pcolor(AinTime[int(itPara30)], cmap=cm.winter)
+cbar = plt.colorbar(cM)
+cbar.set_label('Amplitud onda', rotation=270)
+plt.savefig('Onda30.pdf')
 plt.close()
 
-plt.imshow(AinTime[25])
-plt.colorbar()
-plt.show()
+#para t=60
+fig2 = plt.figure(figsize=plt.figaspect(2.))
+fig2.suptitle('Cuando t=60')
+
+ax = fig2.add_subplot(2,1,1, projection='3d')
+linea = ax.plot_surface(X,Y, AinTime[int(iteraciones)],rstride=1, cstride=1, cmap= cm.winter)
+fig2.colorbar(linea, shrink =0.5, aspect = 5)
+ax.set_zlim(-2,1)
+
+ax = fig2.add_subplot(2,1,2)
+ax.imshow(AinTime[int(iteraciones)], cmap= cm.winter)
+cM = ax.pcolor(AinTime[int(itPara30)], cmap=cm.winter)
+cbar = plt.colorbar(cM)
+cbar.set_label('Amplitud onda', rotation=270)
+plt.savefig('Onda60.pdf')
 plt.close()
 
 
-def anima(i):
-	amplitud = AinTime[i]
-	ax.clear()	
-	linea = ax.plot_surface(X,Y,amplitud, cmap = cm.coolwarm)
-	#linea = plt.imshow(amplitud)
-	ax.set_zlim(-2,1)
-	return linea,
+imAmp =[]
+for i in range(int(iteraciones)):
+	im = plt.imshow(AinTime[i], cmap= cm.winter)
+	imAmp.append([im])
 
-#anim = animation.FuncAnimation(fig, anima, int(iteraciones), interval=25, blit=False)
-#anim.save('Onda.mp4', fps = 20)
+anim = animation.ArtistAnimation(fig, imAmp, interval =20, blit = True, repeat_delay=1000)
+anim.save('Onda.mp4')
+
+
+
+
+
+
 
 
 
