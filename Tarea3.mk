98272cb5c7f4bf457555b473f053653b4090dd26
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-20 21:52:18 -0500

    Este fue

--- Tarea3.mk
+++ Tarea3.mk
@@ -1,18 +1,17 @@
 Resultados_hw3.pdf: Resultados_hw3.tex Onda30.pdf Onda60.pdf sistemaSolar.pdf
 	pdflatex $<
 
+sistemaSolar.pdf: Planetas.c Plots_Planetas.py
+	cc Planetas.c -lm -o Planetas.x
+	./Planetas.x> datos.csv
+	python Plots_Planetas.py
+
 Onda30.pdf: Onda.py
 	python Onda.py
 
 Onda60.pdf: Onda.py
 	python Onda.py
 
-sistemaSolar.pdf: Plots_Planetas.py
-	python Plots_Planetas.py
-
-datos.csv: Planetas.c
-	cc Planetas.c -lm -o Planetas.x
-	./Planetas.x> datos.csv
 
 
 	
